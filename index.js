a3960cf79df0a18a1a125b42c3386d6f944952eb
Furkan <muhammedcengiz1@gmail.com>
2022-09-08 20:31:36 +0300

    Şifrelerin üretileceği arraydeki sembol ve rakamlar checkbox kullanılarak kaldırılabilir hale getirildi ve değişiklikleri kaydedecek buton eklendi.

--- index.js
+++ index.js
@@ -2,13 +2,45 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 ,"c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6"
 , "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
+const justAlphabet = characters.slice(0,52)
+const symbolsRemoved = characters.slice(0, 62)
+const justSymbols = characters.slice(62,91)
+const numbersRemoved = characters.slice(0,52).concat(justSymbols)
+
+
 let pw1 = document.getElementById("pw-1")
 let pw2 = document.getElementById("pw-2")
 let pwInput = document.getElementById("pw-input")
 
 
 
-function generatePasswords() {
+let stateOfCheck = document.querySelectorAll('input[name="slice-array"]:checked')
+
+function saveArray() {
+
+    stateOfCheck = document.querySelectorAll('input[name="slice-array"]:checked')
+    let values = []
+    stateOfCheck.forEach((checkbox) => 
+    {
+    values.push(checkbox.value)
+    });
+    console.log(values)
+
+    if (values.length === 1 && values[0] === 'numbers') {
+        return numbersRemoved;
+    } else if (values.length === 1 && values[0] === "symbols") {
+        return symbolsRemoved;
+    }else if (values.length === 2) {
+        return justAlphabet;
+    } else {
+        return characters
+    }
+}
+
+
+
+
+function generatePasswords(ar = saveArray()) {
     let passwordLength = null
     passwordLength = prompt("Please enter a password length",15)
     console.log(passwordLength)
@@ -20,11 +52,12 @@ function generatePasswords() {
     }
     let password1 =""
     let password2 =""
+    console.log(ar)
     for(let i = 0; i<passwordLength; i++) {
-        let randomIndex1 = Math.floor(Math.random() * characters.length)
-        let randomIndex2 = Math.floor(Math.random() * characters.length)
-        password1 += characters[randomIndex1]
-        password2 += characters[randomIndex2]
+        let randomIndex1 = Math.floor(Math.random() * ar.length)
+        let randomIndex2 = Math.floor(Math.random() * ar.length)
+        password1 += ar[randomIndex1]
+        password2 += ar[randomIndex2]
     }
     setPasswords(password1, password2)
 }
@@ -43,3 +76,4 @@ function copy2() {
     navigator.clipboard.writeText(pw2.textContent)
     alert("Password copied: " + pw2.textContent);
 }
+
